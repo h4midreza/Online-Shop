2d684eb73d5f0ae8f9dde2f4d57a01a7f9d0a14c
Hamidreza <manafi.hamidreza@gmail.com>
2024-03-23 23:23:33 +0330

    Implement Detail page

---
+++ app/src/main/java/com/example/onlineshop/activity/DetailActivity.java
@@ -0,0 +1,141 @@
+package com.example.onlineshop.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.onlineshop.adapter.SizeAdapter;
+import com.example.onlineshop.adapter.SliderAdapter;
+import com.example.onlineshop.databinding.ActivityDetailBinding;
+import com.example.onlineshop.domain.ItemsDomain;
+import com.example.onlineshop.domain.SliderItem;
+import com.example.onlineshop.fragment.DescriptionFragment;
+import com.example.onlineshop.fragment.ReviewFragment;
+import com.example.onlineshop.fragment.SoldFragment;
+import com.example.onlineshop.helper.ManagementCart;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DetailActivity extends AppCompatActivity {
+    ActivityDetailBinding binding;
+    private ItemsDomain object;
+    private int numberOrder = 1;
+    private ManagementCart managementCart;
+    private Handler sliderHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        managementCart = new ManagementCart(this);
+
+        getBundles();
+        initBanners();
+        initSize();
+        setupViewPager();
+    }
+
+    private void initSize() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("S");
+        list.add("M");
+        list.add("L");
+        list.add("XL");
+        list.add("XXL");
+
+        binding.recyclerSize.setAdapter(new SizeAdapter(list));
+        binding.recyclerSize.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+    }
+
+    private void initBanners() {
+        ArrayList<SliderItem> sliderItem = new ArrayList<>();
+        for (int i = 0; i < object.getPicUrl().size(); i++) {
+            sliderItem.add(new SliderItem(object.getPicUrl().get(i)));
+        }
+        binding.viewpagerSlider.setAdapter(new SliderAdapter(sliderItem, binding.viewpagerSlider));
+        binding.viewpagerSlider.setClipToPadding(false);
+        binding.viewpagerSlider.setClipChildren(false);
+        binding.viewpagerSlider.setOffscreenPageLimit(3);
+        binding.viewpagerSlider.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+    }
+
+    private void getBundles() {
+        object = (ItemsDomain) getIntent().getSerializableExtra("object");
+
+        binding.titleTxt.setText(object.getTitle());
+        binding.priceTxt.setText("$" + object.getPrice());
+        binding.ratingBar.setRating((float) object.getRating());
+        binding.ratingTxt.setText(object.getRating() + "Rating");
+
+        binding.addToCartBtn.setOnClickListener(view -> {
+            object.setNumberInCart(numberOrder);
+            managementCart.insertItem(object);
+        });
+        binding.backBtn.setOnClickListener(view -> finish());
+    }
+
+    private void setupViewPager(){
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        DescriptionFragment tab1 = new DescriptionFragment();
+        ReviewFragment tab2 = new ReviewFragment();
+        SoldFragment tab3 = new SoldFragment();
+
+        Bundle bundle1 = new Bundle();
+        Bundle bundle2 = new Bundle();
+        Bundle bundle3 = new Bundle();
+
+        bundle1.putString("description", object.getDescription());
+
+        tab1.setArguments(bundle1);
+        tab2.setArguments(bundle2);
+        tab3.setArguments(bundle3);
+
+        adapter.addFrag(tab1, "Description");
+        adapter.addFrag(tab2, "Review");
+        adapter.addFrag(tab3, "Sold");
+
+        binding.viewpager.setAdapter(adapter);
+        binding.tabLayout.setupWithViewPager(binding.viewpager);
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        private void addFrag(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+}
